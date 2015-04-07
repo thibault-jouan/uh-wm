4051a10612e52c6a338ed604613da62f0b86823e
Thibault Jouan <tj@a13.fr>
2015-04-07 17:32:47 +0000

    Declare Uh::WM own error classes

--- lib/uh/wm.rb
+++ lib/uh/wm.rb
@@ -1,3 +1,11 @@
 require 'uh'
 
 require 'uh/wm/cli'
+
+module Uh
+  module WM
+    Error         = Class.new(StandardError)
+    RuntimeError  = Class.new(RuntimeError)
+    ArgumentError = Class.new(Error)
+  end
+end
