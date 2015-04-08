01d780576042393be58e04da6b1cb1c0e9d2c78e
Thibault Jouan <tj@a13.fr>
2015-04-08 16:45:59 +0000

    Add uhwm_wait_output cucumber helper

--- features/steps/output_steps.rb
+++ features/steps/output_steps.rb
@@ -1,3 +1,14 @@
+def uhwm_wait_output message, timeout: 1
+  Timeout.timeout(timeout) do
+    loop do
+      break if assert_partial_output_interactive message
+      sleep 0.1
+    end
+  end
+rescue Timeout::Error
+  fail "expected message `#{message}' not seen after #{timeout} seconds"
+end
+
 Then /^the output must contain exactly the usage$/ do
   assert_exact_output <<-eoh, all_output
 Usage: uhwm [options]
--- features/steps/x_steps.rb
+++ features/steps/x_steps.rb
@@ -1,10 +1,5 @@
 Then /^it must connect to X display$/ do
-  Timeout.timeout(exit_timeout) do
-    loop do
-      break if assert_partial_output_interactive 'Connected to'
-      sleep 0.1
-    end
-  end
+  uhwm_wait_output 'Connected to'
   expect(`sockstat -u`.lines.grep /\s+ruby.+\s+#{@process.pid}/)
     .not_to be_empty
 end
