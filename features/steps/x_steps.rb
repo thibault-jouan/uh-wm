366028fa01c0d3d452419e52acd30d705fd14453
Thibault Jouan <tj@a13.fr>
2015-04-09 02:29:52 +0000

    Allow X related tests to work on travis CI

--- features/steps/x_steps.rb
+++ features/steps/x_steps.rb
@@ -2,12 +2,20 @@ def x_key key
   fail "cannot simulate X key `#{key}'" unless system "xdotool key #{key}"
 end
 
+def x_socket_check pid
+  case RbConfig::CONFIG['host_os']
+  when /linux/
+    `netstat -xp`.lines.grep /\s+#{pid}\/ruby/
+  else
+    `sockstat -u`.lines.grep /\s+ruby.+\s+#{pid}/
+  end.any?
+end
+
 When /^I press the default quit key binding$/ do
   x_key 'alt+q'
 end
 
 Then /^it must connect to X display$/ do
   uhwm_wait_output 'Connected to'
-  expect(`sockstat -u`.lines.grep /\s+ruby.+\s+#{@process.pid}/)
-    .not_to be_empty
+  expect(x_socket_check @process.pid).to be true
 end
