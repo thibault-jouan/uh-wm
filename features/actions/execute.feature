1e1645107c35b8569c772872273d22dc8be92abe
Thibault Jouan <tj@a13.fr>
2015-04-16 17:12:00 +0000

    Implement `execute' action keyword

---
+++ features/actions/execute.feature
@@ -0,0 +1,9 @@
+Feature: `execute' action keyword
+
+  Scenario: executes the given command in a shell with current standard output
+    Given uhwm is running with this run control file:
+      """
+      key(:f) { execute 'echo etucexe_tset | rev' }
+      """
+    When I press the alt+f keys
+    Then the output must contain "test_execute"
--- lib/uh/wm/actions_handler.rb
+++ lib/uh/wm/actions_handler.rb
@@ -12,6 +12,21 @@ module Uh
       def quit
         @events.emit :quit
       end
+
+      def execute command
+        @env.log "Execute: #{command}"
+        pid = fork do
+          fork do
+            Process.setsid
+            begin
+              exec command
+            rescue Errno::ENOENT => e
+              @env.log_error "ExecuteError: #{e}"
+            end
+          end
+        end
+        Process.waitpid pid
+      end
     end
   end
 end
