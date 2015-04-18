416951dcc2ca30f9f2f692c8c78c1a3cf9314256
Thibault Jouan <tj@a13.fr>
2015-04-18 03:10:26 +0000

    Remove comments left in Workers::Blocking

--- lib/uh/wm/workers/blocking.rb
+++ lib/uh/wm/workers/blocking.rb
@@ -3,10 +3,6 @@ module Uh
     module Workers
       class Blocking < Base
         def work_events
-          #until yield
-          #  @on_events_read_bang.call
-          #end
-          #@on_events_read_bang.call until yield
           @on_read_next.call
         end
       end
