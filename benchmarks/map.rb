1d040d67835c953b6194028131bc88ba69bcb791
Thibault Jouan <tj@a13.fr>
2015-05-19 01:07:47 +0000

    Prototype mapping benchmark

---
+++ benchmarks/map.rb
@@ -0,0 +1,38 @@
+require 'benchmark'
+require 'headless'
+require 'uh/wm'
+require 'uh/wm/testing/x_client'
+
+include Uh::WM::Testing::AcceptanceHelpers
+
+n = 100
+
+def gets_nonblock io
+  buffer = ''
+  buffer << io.read_nonblock(1) while buffer[-1] != "\n"
+  buffer
+rescue IO::EAGAINWaitReadable
+  sleep 0.1
+  retry
+end
+
+Headless.ly do
+  io = IO.popen(%w[uhwm -v -f /dev/null])
+  loop do
+    break if gets_nonblock(io).include? 'Working events'
+  end
+
+  cl = XClient.new
+  Benchmark.benchmark do |bm|
+    bm.report do
+      n.times do
+        cl.map.unmap
+      end
+    end
+  end
+  cl.destroy.terminate
+
+  fail 'cannot quit uhwm' unless system 'xdotool key alt+shift+q'
+  Process.wait(io.pid)
+  puts io.read
+end
